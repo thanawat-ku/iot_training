7a9d8a75898ff9acfa0377a3368c718d82b7dc09
Fook <59011305+thanawat-ku@users.noreply.github.com>
2024-09-04 14:38:32 +0700

    Update lab1.ino

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,23 +1,11 @@
-int redPin = 5;
-int greenPin = 18;
-int yellowPin = 19;
+int ledPin = 5;
 void setup(){
-    pinMode(redPin, OUTPUT);
-    pinMode(greenPin, OUTPUT);
-    pinMode(yellowPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
 }
 
 void loop(){
-    digitalWrite(greenPin, HIGH);
-    digitalWrite(yellowPin, LOW);
-    digitalWrite(redPin, LOW);
-    delay(5000);
-    digitalWrite(greenPin, LOW);
-    digitalWrite(yellowPin, HIGH);
-    digitalWrite(redPin, LOW);
-    delay(2000);
-    digitalWrite(greenPin, LOW);
-    digitalWrite(yellowPin, LOW);
-    digitalWrite(redPin, HIGH);
-    delay(6000);
+    digitalWrite(ledPin, HIGH);
+    delay(1000);    
+    digitalWrite(ledPin, LOW);
+    delay(1000);    
 }
