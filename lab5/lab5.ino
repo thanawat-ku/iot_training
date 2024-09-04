ffd8ade01b0407a9ecb1a8d84dcb5d561b5de78f
Fook <59011305+thanawat-ku@users.noreply.github.com>
2024-09-04 14:49:11 +0700

    Update lab5.ino

--- lab5/lab5.ino
+++ lab5/lab5.ino
@@ -1,24 +1,44 @@
-int analogPin = 32;
-int val = 0;
-int redPin = 5;
-int greenPin = 18;
+//ลงทะเบียนขอรหัส token ที่ https://notify-bot.line.me/my/
+
+#include <WiFi.h> // นำเข้าไลบรารี่ WiFi
+#include <ArtronShop_LineNotify.h> // นำเข้าไลบารี่ ArtronShop_LineNotify
+
+const char* ssid = "WiFi Name"; // ชื่อ WiFi
+const char* password = "WiFi Password"; // รหัสผ่าน WiFi
+#define LINE_TOKEN "LINE Token" // LINE Token
+
 void setup() {
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(115200); // เริ่มต้นใช้ Serial ที่ความเร็ว 115200
+  while (!Serial) { delay(100); }
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println("******************************************************");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password); // เริ่มต้นเชื่อมต่อ WiFi
+
+  while (WiFi.status() != WL_CONNECTED) { // วนลูปหากยังเชื่อมต่อ WiFi ไม่สำเร็จ
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  LINE.begin(LINE_TOKEN); // เริ่มต้นใช้ LINE Notify
+
+  if (LINE.send("รถโดนขโมย")) {  // ถ้าส่งข้อความ "รถโดนขโมย" ไปที่ LINE สำเร็จ
+    Serial.println("Send notify successful"); // ส่งข้อความ "Send notify successful" ไปที่ Serial Monitor
+  } else { // ถ้าส่งไม่สำเร็จ
+    Serial.printf("Send notify fail. check your token (code: %d)\n", LINE.status_code); // ส่งข้อความ "Send notify fail" ไปที่ Serial Monitor
+  }
 }
 
 void loop() {
-  val = analogRead(analogPin);
-  Serial.print("val = "); 
-  Serial.println(val); 
-  if(val>=1000){
-    digitalWrite(greenPin, HIGH);
-    digitalWrite(redPin, LOW);
-  }
-  else{
-    digitalWrite(greenPin, LOW);
-    digitalWrite(redPin, HIGH);
-  }
-  delay(500);
+
 }
